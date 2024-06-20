89ac66a1a2f00296c780b56cc2d7fe55080825ca
Alexis D'Andrea <56324802+adandrea8@users.noreply.github.com>
2024-06-19 22:16:52 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 	ini_set('display_errors',1);
 	error_reporting(-1);
-	define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASSWORD', 'root');
-    define('DB_DATABASE', 'idukan');
-?>
+	define('DB_HOST', 'db_endpoint');
+    define('DB_USER', 'db_user');
+    define('DB_PASSWORD', 'db_password');
+    define('DB_DATABASE', 'database_name');
+?>% 

1b5bd786d17cbcb6c60f00bf0018fd4c8e4f97b4
Alexis D'Andrea <56324802+adandrea8@users.noreply.github.com>
2024-06-20 22:51:08 -0300

    Cambio de las variables en config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 	ini_set('display_errors',1);
 	error_reporting(-1);
-	define('DB_HOST', 'db_endpoint');
-    define('DB_USER', 'db_user');
-    define('DB_PASSWORD', 'db_password');
-    define('DB_DATABASE', 'database_name');
+	define('DB_HOST', 'obligatorio-db');
+    define('DB_USER', 'admin');
+    define('DB_PASSWORD', 'password');
+    define('DB_DATABASE', 'obligatorio-db');
 ?>% 
